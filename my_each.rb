b126fc5115487261a8db0dda54c8d5e4d2a4cb74
mjones1818 <michael.jones0532@gmail.com>
2020-10-02 12:51:29 -0500

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (array)
+  array_size = array.length
+  counter = 0
+  while counter < array_size
+    yield(array[counter])
+    counter +=1
+  end
+  array
 end
+
+my_each([1,2,3,4,5]) {|i| i * 2}
+
+# ruby my_each.rb
